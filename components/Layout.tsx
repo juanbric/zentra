2795ccc24a82dc7bfe10cbe506dfcf78acdbf279
Juan Briceno <87806225+juanbric@users.noreply.github.com>
2023-01-09 16:57:52 +0100

    Staging (#13)
    * navbar and layout improvements
    
    * presentanos tu proyecto

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,10 +4,10 @@ import { Footer } from "./Footer";
 export default function Layout({ children }: { children: any }) {
   return (
     <>
-      <AppBar />
-      <div className="main">
-        <div className="grid-perfect px-8 lg:px-0">{children}</div>
-      </div>
+       <AppBar />
+      <div className="lg:flex lg:justify-center lg:items-center">
+        <div className="px-4 lg:px-8 w-auto lg:w-[1130px] ">{children}</div>
+        </div>
       <Footer />
     </>
   );
