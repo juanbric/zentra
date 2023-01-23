056d81a2746f437b4e24e4fb1026372d4609bd0f
Juan Briceno <juanbri@gmx.com>
2023-01-23 17:41:18 +0000

    icons

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -12,19 +12,19 @@ const Banner = ({
       <div className="px-4 lg:px-8 w-auto lg:w-[1130px]">
         <section className="">
           <div className="md:grid md:grid-cols-2 md:gap-[80px]">
-            <div className="py-[52px] md:py-[128px]">
+            <div className="py-[52px] md:py-[138px]">
               <div className="title mb-2">{headerTitle}</div>
               <div className="sub-title mb-6">{headerSubTitle}</div>
               <Link
                 className="hover:text-[#0d0d0d] bg-[#3c31dd] hover:bg-[#fffffb] py-2 rounded-[6px] px-4"
-                href={"/about"}
+                href={"/contacto"}
               >
                 Habla con nosotros
               </Link>
             </div>
-            <div className="hidden md:mt-0 md:flex pb-[52px] max-w-[381px] md:py-[128px] md:pb-0 relative">
+            <div className="hidden md:mt-0 md:flex pb-[52px] max-w-[381px] md:py-[138px] md:pb-0 relative">
               <img src="/white.png" className="ml-12 rounded-[1000px] max-w-[381px] h-[381px]" />
-              <img src="/blue.png" className="rounded-[1000px] w-[381px] h-[381px] z-10 absolute md:top-[77px] md:left-[155px]" />
+              <img src="/blue.png" className="rounded-[1000px] w-[381px] h-[381px] z-10 absolute md:top-[87px] md:left-[155px]" />
             </div>
           </div>
         </section>
