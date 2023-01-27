f4fa7dcaa66353519aa5a556159b79bb232c26f0
Juan Briceno <juanbri@gmx.com>
2023-01-27 20:45:24 +0000

    styles

--- pages/contacto.tsx
+++ pages/contacto.tsx
@@ -1,7 +1,34 @@
 import ContactForm from "../components/ContactForm";
+import MetaTag from "../components/MetaTag";
+import Schema from "../components/Schema";
 
 export const Contacto = () => {
-  return <ContactForm />;
+  const imageMeta = "https://i.ibb.co/1sJSnJN/zentra.png";
+  const description =
+    "Lleva tu negocio al siguiente nivel con nuestra agencia de desarrollo web en Las Palmas de Gran Canaria. Especialistas en crear soluciones personalizadas para impulsar tu crecimiento digital. ";
+  const title = "Cuéntanos | zentradev | Agencia de Desarrollo Web";
+  const date = new Date();
+
+  return (
+    <>
+      <Schema
+        title={title}
+        date={date}
+        image={imageMeta}
+        articleBody={undefined}
+        description={description}
+      />
+      <MetaTag
+        title={title}
+        description={description}
+        url={URL}
+        image={imageMeta}
+      />
+      <main className="pt-[100px] pb-[80px]">
+        <ContactForm />
+      </main>
+    </>
+  );
 };
 
 export default Contacto;
